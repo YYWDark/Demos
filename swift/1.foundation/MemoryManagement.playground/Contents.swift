9d3d7ca436d73c0255d6fcafae2111a013de4e28
wyy <wangyayun0629@163.com>
2017-06-06 23:36:27 +0800

    update

--- swift/1.foundation/MemoryManagement.playground/Contents.swift
+++ swift/1.foundation/MemoryManagement.playground/Contents.swift
@@ -4,6 +4,7 @@ import UIKit
 
 class User {
     var name: String
+    var subscriptions: [CarrierSubscription] = []
     private(set) var phones: [Phone] = []
     func add(phone: Phone) {
         phones.append(phone)
@@ -25,6 +26,15 @@ class User {
 class Phone {
     let model: String
     var owner: User?
+    var carrierSubscription: CarrierSubscription?
+    
+    func provision(carrierSubscription: CarrierSubscription) {
+        self.carrierSubscription = carrierSubscription
+    }
+    
+    func decommission() {
+        self.carrierSubscription = nil
+    }
     
     init(model: String) {
         self.model = model
@@ -36,10 +46,33 @@ class Phone {
     }
 }
 
+class CarrierSubscription {
+    let name: String
+    let countryCode: String
+    let number: String
+    let user: User
+    
+    init(name: String, countryCode: String, number: String, user: User) {
+        self.name = name
+        self.countryCode = countryCode
+        self.number = number
+        self.user = user
+        
+        user.subscriptions.append(self)
+        print("CarrierSubscription \(name) is initialized")
+    }
+    
+    deinit {
+        print("CarrierSubscription \(name) is being deallocated")
+    }
+}
+
 do {
     let user1 = User(name: "John")
     let iPhone = Phone(model: "iPhone 6s Plus")
-//    user1.add(phone: iPhone)
+    user1.add(phone: iPhone)
+    let subscription1 = CarrierSubscription(name: "TelBel", countryCode: "0032", number: "31415926", user: user1)
+    iPhone.provision(carrierSubscription: subscription1)
 }
 
 var str = "Hello, playground"
